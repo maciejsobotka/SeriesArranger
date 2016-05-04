d0efd69b706f0870a1aa9c92a29d569dba85fcb0
maciejsobotka <sobot92@tlen.pl>
2016-05-04 19:57:21 +0200

    Initial commit.

---
+++ SeriesArranger/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SeriesArranger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.buttonFolderSelect = new System.Windows.Forms.Button();
+            this.buttonArrange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(12, 12);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(172, 20);
+            this.textBoxSource.TabIndex = 0;
+            // 
+            // buttonFolderSelect
+            // 
+            this.buttonFolderSelect.Location = new System.Drawing.Point(197, 10);
+            this.buttonFolderSelect.Name = "buttonFolderSelect";
+            this.buttonFolderSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonFolderSelect.TabIndex = 1;
+            this.buttonFolderSelect.Text = "Folder";
+            this.buttonFolderSelect.UseVisualStyleBackColor = true;
+            this.buttonFolderSelect.Click += new System.EventHandler(this.buttonFolderSelect_Click);
+            // 
+            // buttonArrange
+            // 
+            this.buttonArrange.Location = new System.Drawing.Point(12, 50);
+            this.buttonArrange.Name = "buttonArrange";
+            this.buttonArrange.Size = new System.Drawing.Size(260, 36);
+            this.buttonArrange.TabIndex = 2;
+            this.buttonArrange.Text = "Arrange!";
+            this.buttonArrange.UseVisualStyleBackColor = true;
+            this.buttonArrange.Click += new System.EventHandler(this.buttonArrange_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 113);
+            this.Controls.Add(this.buttonArrange);
+            this.Controls.Add(this.buttonFolderSelect);
+            this.Controls.Add(this.textBoxSource);
+            this.Name = "MainForm";
+            this.Text = "SeriesArranger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.Button buttonFolderSelect;
+        private System.Windows.Forms.Button buttonArrange;
+    }
+}
+
---
+++ SeriesArranger/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeriesArranger
+{
+    public partial class MainForm : Form
+    {
+        private string directoryPath = "";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFolderSelect_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+            directoryPath = folderBrowserDialog.SelectedPath;
+            textBoxSource.Text = directoryPath;
+        }
+
+        private void buttonArrange_Click(object sender, EventArgs e)
+        {
+            if (directoryPath != "")
+            {
+                textBoxSource.Text = directoryPath;
+                string[] fileEntries = Directory.GetFiles(directoryPath);
+                string[] directories = Directory.GetDirectories(directoryPath);
+                foreach (String file in fileEntries)
+                {
+                    if (file.EndsWith(".mp4") || file.EndsWith(".avi") || file.EndsWith(".mkv"))
+                    {
+                        textBoxSource.Text = "in!";
+                    }
+                }
+            }
+        }
+    }
+}
