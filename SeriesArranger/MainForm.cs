63df97aa485890c664c500bfb5a5152a9eb2b8ad
maciejsobotka <sobot92@tlen.pl>
2016-05-04 20:52:33 +0200

    Working version.

--- SeriesArranger/MainForm.cs
+++ SeriesArranger/MainForm.cs
@@ -28,19 +28,46 @@ namespace SeriesArranger
 
         private void buttonArrange_Click(object sender, EventArgs e)
         {
-            if (directoryPath != "")
+            if (Directory.Exists(directoryPath))
             {
-                textBoxSource.Text = directoryPath;
-                string[] fileEntries = Directory.GetFiles(directoryPath);
-                string[] directories = Directory.GetDirectories(directoryPath);
-                foreach (String file in fileEntries)
+                string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                string[] seriesNames = File.ReadAllLines(path + "\\seriesNames.txt");
+                string[] fileEntries = Directory.GetFiles(directoryPath)
+                                        .Select(file => Path.GetFileName(file)).ToArray();
+                string[] directories = Directory.GetDirectories(directoryPath)
+                                        .Select(dir => Path.GetFileName(dir)).ToArray();
+                foreach(var name in seriesNames)
                 {
-                    if (file.EndsWith(".mp4") || file.EndsWith(".avi") || file.EndsWith(".mkv"))
+                    string nameDotted = name.Replace(' ', '.');
+                    string nameLower = name.ToLower();
+                    string nameDottedLower = name.Replace(' ', '.').ToLower();
+                    Parallel.ForEach(fileEntries, file =>
                     {
-                        textBoxSource.Text = "in!";
-                    }
+                        if (file.EndsWith(".mp4") || file.EndsWith(".avi") || file.EndsWith(".mkv"))
+                            if (file.Contains(name) || file.Contains(nameDotted)
+                                || file.Contains(nameLower) || file.Contains(nameDottedLower))
+                            {
+                                if (!Directory.Exists(directoryPath + "\\" + name))
+                                    Directory.CreateDirectory(directoryPath + "\\" + name);
+                                File.Move(directoryPath + "\\" + file, directoryPath + "\\" + name + "\\" + file);
+                            }
+                    });
+                    Parallel.ForEach(directories, directory =>
+                    {
+                        if (directory.Contains(name) || directory.Contains(nameDotted)
+                                || directory.Contains(nameLower) || directory.Contains(nameDottedLower))
+                        {
+                            if (!Directory.Exists(directoryPath + "\\" + name))
+                                Directory.CreateDirectory(directoryPath + "\\" + name);
+                            if (directory != name)
+                                Directory.Move(directoryPath + "\\" + directory, directoryPath + "\\" + name + "\\" + directory);
+                        }
+                    });
                 }
+                MessageBox.Show("Done!", "Series arranged");
             }
+            else
+                MessageBox.Show("Incorrect directory!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
