c9e88ef7a5cbb19e06aa04dc9ece826ccd0be0ed
maciejsobotka <sobot92@tlen.pl>
2016-05-10 00:32:51 +0200

    Added lowercase prepositions cases.
    Added check weather file/directory still exists before Move.

--- SeriesArranger/MainForm.cs
+++ SeriesArranger/MainForm.cs
@@ -40,26 +40,36 @@ namespace SeriesArranger
                     string nameDotted = name.Replace(' ', '.');
                     string nameLower = name.ToLower();
                     string nameDottedLower = name.Replace(' ', '.').ToLower();
+                    string namePrepositionsToLow = name.Replace("The", "the").Replace("In", "in").Replace("At", "at");
+                    string nameDottedPrepositionsToLow = name.Replace(' ', '.').Replace("The", "the").Replace("In", "in").Replace("At", "at");
                     Parallel.ForEach(fileEntries, file =>
                     {
                         if (file.EndsWith(".mp4") || file.EndsWith(".avi") || file.EndsWith(".mkv"))
                             if (file.Contains(name) || file.Contains(nameDotted)
-                                || file.Contains(nameLower) || file.Contains(nameDottedLower))
+                                || file.Contains(nameLower) || file.Contains(nameDottedLower)
+                                || file.Contains(namePrepositionsToLow) || file.Contains(nameDottedPrepositionsToLow))
                             {
-                                if (!Directory.Exists(directoryPath + "\\" + name))
-                                    Directory.CreateDirectory(directoryPath + "\\" + name);
-                                File.Move(directoryPath + "\\" + file, directoryPath + "\\" + name + "\\" + file);
+                                if (File.Exists(directoryPath + "\\" + file))       // else some seriesName is part of another seriesName
+                                {                                                   // exmpl "Fear The Walking Dead" & "The Walking Dead"
+                                    if (!Directory.Exists(directoryPath + "\\" + name))
+                                        Directory.CreateDirectory(directoryPath + "\\" + name);
+                                    File.Move(directoryPath + "\\" + file, directoryPath + "\\" + name + "\\" + file);
+                                }
                             }
                     });
                     Parallel.ForEach(directories, directory =>
                     {
                         if (directory.Contains(name) || directory.Contains(nameDotted)
-                                || directory.Contains(nameLower) || directory.Contains(nameDottedLower))
+                                || directory.Contains(nameLower) || directory.Contains(nameDottedLower)
+                                || directory.Contains(namePrepositionsToLow) || directory.Contains(nameDottedPrepositionsToLow))
                         {
-                            if (!Directory.Exists(directoryPath + "\\" + name))
-                                Directory.CreateDirectory(directoryPath + "\\" + name);
-                            if (directory != name)
-                                Directory.Move(directoryPath + "\\" + directory, directoryPath + "\\" + name + "\\" + directory);
+                            if (Directory.Exists(directoryPath + "\\" + directory)) // else some seriesName is part of another seriesName
+                            {                                                       // exmpl "Fear The Walking Dead" & "The Walking Dead"
+                                if (!Directory.Exists(directoryPath + "\\" + name))
+                                    Directory.CreateDirectory(directoryPath + "\\" + name);
+                                if (directory != name)
+                                    Directory.Move(directoryPath + "\\" + directory, directoryPath + "\\" + name + "\\" + directory);
+                            }
                         }
                     });
                 }
